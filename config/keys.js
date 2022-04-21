526d47424775678902610aa2a4fb513f91a8215f
Kaz <82215788+Kazdev0@users.noreply.github.com>
2022-04-21 17:17:59 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports ={
-    mongoURI: 'mongodb+srv://user:rcon@cluster0.5fqws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    discordTOKEN: 'OTY1OTEyNDE4MTYzMjUzMjY4.Yl6GCg.In3JWANLWq4kbTsW-SYi4rj9FvY',
+    mongoURI: 'm.ongodb+srv://user:rcon@cluster0.5fqws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    discordTOKEN: 'O.TY1OTEyNDE4MTYzMjUzMjY4.Yl6GCg.In3JWANLWq4kbTsW-SYi4rj9FvY',
     botOWNER: "952940471368450068"
 }
